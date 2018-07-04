3b6798574d6ee3c5e17a9202cd4af8d117100a8f
yangxuechen <1324688478@qq.com>
2018-07-04 22:08:23 +0800

    update UserMapper

--- src/mybatis/chapter1/mapper/UserMapper.java
+++ src/mybatis/chapter1/mapper/UserMapper.java
@@ -1,10 +1,18 @@
 package mybatis.chapter1.mapper;
 
+import java.util.List;
+
 import mybatis.chapter1.bean.User;
 
 public interface UserMapper {
 	//添加用户
 	public int insertUser(User user);
-    //删除用户
+    //通过用户id删除用户
 	public int deleteUser(int id);
+	//更新用户信息
+	public int updateUser(User user);
+	//通过用户id查找用户
+	public User getUser(int id);
+	//查找所有用户
+	public List<User> getAllUsers();
 }
