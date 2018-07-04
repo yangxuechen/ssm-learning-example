f05cde2f3223eb47bf6ce993d519499c98f265a5
yangxuechen <1324688478@qq.com>
2018-07-04 21:33:37 +0800

    create User.java

---
+++ src/mybatis/chapter1/bean/User.java
@@ -0,0 +1,19 @@
+package mybatis.chapter1.bean;
+
+public class User {
+	private String userName;  //用户姓名
+	private String password;  //用户密码
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
